ec8f14583d1b5559c92af483caca1025d87aef55
Sebastien31-Dev <sebastien.bages@outlook.fr>
2020-10-02 10:10:07 +0200

    Script added for margin auto between main and nav

--- js/cards.js
+++ js/cards.js
@@ -371,7 +371,13 @@ for (let i = 0; i < checkbox.length; i++) {
 		
 	}, false);
 }
-	
+
+//TAILLE BALISE MAIN DYNAMIQUE
+const nav = document.querySelector('nav');
+let height = parseFloat(nav.clientHeight);
+
+const main = document.querySelector('main');
+main.style.marginTop = height + "px";
 
 /*
 END FONCTIONS FOR CARDS.HTML
